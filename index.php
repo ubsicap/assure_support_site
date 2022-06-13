a7b7ecee421b8dd0622125058199ebe6c4500fde
Daniel March <daniel_march@sil.org>
2022-06-13 09:33:38 -0400

    Test push
    just a test

--- index.php
+++ index.php
@@ -25,3 +25,5 @@
 define('QA_BASE_DIR', dirname(empty($_SERVER['SCRIPT_FILENAME']) ? __FILE__ : $_SERVER['SCRIPT_FILENAME']) . '/');
 
 require 'qa-include/qa-index.php';
+
+//testing to see if I have push accesss
