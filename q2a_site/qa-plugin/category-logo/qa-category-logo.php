7396220dcdda806fda54bedabcb094b81b18e31d
Ling <lingxin_chen@sil.org>
2022-06-27 15:45:01 -0400

    Store the input of category-logo plugin

--- q2a_site/qa-plugin/category-logo/qa-category-logo.php
+++ q2a_site/qa-plugin/category-logo/qa-category-logo.php
@@ -1,27 +1,22 @@
 <?php
 
 class qa_category_logo {
-    
-    public function option_default($option)
-	{
-		if ($option === 'category_logo_url')
-			return '';
-	}
 
     public function admin_form(&$qa_content)
 	{
+        //$this->debug_to_console($qa_content);
+        $urls = $this->get_categories();
+        $categories_amount = count($urls);
         $saved = qa_clicked('category_logo_save_button');
-
 		if ($saved) {
 			qa_opt('category_logo_on', (int) qa_post_text('category_logo_on_field'));
-			qa_opt('category_logo_url', qa_post_text('category_logo_url_field'));
+            for($i=0;$i<$categories_amount;$i++) {
+            	qa_opt('category_logo_url' .$i.'', qa_post_text('category_logo_url_field'.$i.''));
+            }
 		}
 
-		// qa_set_display_rules($qa_content, array(
-		// 	'category_logo_url_display' => 'category_logo_url_field',
-		// ));
-        
-        $urls = $this->get_categories();
+        $this -> qa_set_display_rules_helper($categories_amount);
+
         //make array consists of prompt and specific labels with corresponding categories
         $fields = array(array(
             'label' => 'Add specific logo to corresponding categories',
@@ -44,23 +39,32 @@ class qa_category_logo {
 		);
     }
 
-    public function get_categories() {
+    private function get_categories() {
         $categories = array();
-        $results = qa_db_query_raw( //get the titles of categories from db
-			'SELECT title From qa_categories'
-		);
         $i = 0;
+        $results = qa_db_query_raw( //get the titles of categories from db
+            'SELECT title From qa_categories'
+        );
         //make array consists of specific labels with corresponding categories
         foreach($results as $result) {
+            $tag_name = "category_logo_url_field" .$i. "";
             $categories[$i] = array( 
                 'id' => 'category_logo_url_display' .$i. '',
                 'label' => 'Url for logo for ' .$result['title']. ':',
                 'type' => 'text',
                 'value' => qa_opt('category_logo_url'.$i. ''),
-                'tags' => 'name="category_logo_url_field".$i. ""',
+                'tags' => 'name=' .$tag_name. "",
             );
             $i++;
         }
         return $categories;
+    }
+
+    private function qa_set_display_rules_helper($categories_amount ) {
+        $array = array();
+        for($i=0;$i<$categories_amount;$i++) {
+            $array += array('category_logo_url_display' .$i. '' => 'category_logo_url_field'.$i.'');
+        }
+        qa_set_display_rules($qa_content, $array);
     }
 }
