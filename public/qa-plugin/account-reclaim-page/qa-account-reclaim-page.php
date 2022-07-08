c7861a508ca7ca4b92cd7a2ba9d5da14d59afb8c
Danny Hammer <daniel_hammer@sil.org>
2022-07-08 14:06:37 -0400

    chore: Initial plugin skeleton

---
+++ public/qa-plugin/account-reclaim-page/qa-account-reclaim-page.php
@@ -0,0 +1,70 @@
+<?php
+/*
+        File: qa-plugin/account-reclaim/qa-account-reclaim.php
+        Description: Page plugin for Account Reclaim process
+*/
+
+class qa_account_reclaim
+{
+        function match_request($request)
+        {
+                /*
+                Should return true if your page module will respond to Q2A page $request.
+                */
+                if (false) {
+                        return true;
+                }
+
+                return false;
+        }
+        function suggest_requests()
+        {
+                /*
+                Should return an array of suggested pages for your module.
+                These suggestions will be displayed within the Q2A admin interface.
+                Each element for a page in the array is itself an array, containing the following elements:
+                        - title contains a human-readable title used to describe the page, e.g. 'More Stats'.
+                        - request contains the Q2A $request string for the page, e.g. 'stats/more'.
+                                Your match_request() function should of course return true for this string.
+                        - nav contains a suggestion about where this page should be linked in the navigation menus.
+                                This is only a hint and can easily be changed by the site's administrator.
+                                Use 'M' for after the main menu, 'B' for before the main menu,
+                                'O' for opposite the main menu, 'F' for the footer, or null for no navigation element.
+                */
+                return array(
+                        array(
+                                'title' => 'Account Reclaim', // title of page
+                                'request' => 'accountreclaim', // request name
+                                'nav' => 'M', // 'M'=main, 'F'=footer, 'B'=before main, 'O'=opposite main, null=none
+                        ),
+                );
+        }
+
+        function process_request($request)
+        {
+                /*
+                Allows your page module to respond for a particular $request.
+                This function should perform any necessary processing based on user inputs,
+                and return information about the page to be displayed in a nested Q2A content array,
+                named $qa_content by convention.
+                (If your function outputs some special content such as XML,
+                return null to prevent Q2A displaying anything further.)
+                It is beyond the scope of this documentation to describe $qa_content in detail
+                        - there are many examples in the qa-page-*.php files and the page plugins that come with Q2A.
+                You can also see how $qa_content is picked apart at the end of qa-page.php and in main() in qa-theme-base.php.
+
+                Some particular points to note:
+                        Always start by assigning $qa_content=qa_content_prepare(); to include navigation menus and the like.
+                        All elements within the nested $qa_content array should be made HTML safe - use qa_html() as appropriate.
+                        Use $qa_content['title'] and $qa_content['error'] to display a page title or error message respectively.
+                        To add some custom HTML to the page, add an element to $qa_content whose key begins with custom, e.g. $qa_content['custom'] or $qa_content['custom_2']. The value of the element should contain the actual HTML.
+                        To add a Q2A-style form to the page, add an element to $qa_content whose key begins with form, e.g. $qa_content['form'] or $qa_content['form_2']. The value of the element should contain a Q2A form array, whose definition is also beyond the scope of this documentation, although see the comments here.
+                        Other possible types of structured content in $qa_content are question lists (keys starting q_list), question views (q_view), answer lists (a_list) and rankings (ranking). Look through the qa-page-*.php files for examples.
+                        The elements in $qa_content are displayed in array order, with a few exceptions such as page titles and errors.
+                */
+
+                $qa_content = qa_content_prepare();
+
+                return $qa_content;
+        }
+}
---
+++ public/qa-plugin/account-reclaim-page/qa-plugin.php
@@ -0,0 +1,9 @@
+<?php
+
+if (!defined('QA_VERSION')) { // don't allow this page to be requested directly from browser
+    header('Location: ../../');
+    exit;
+}
+
+
+qa_register_plugin_module('page', 'qa-account-reclaim.php', 'qa_account_reclaim', 'Account Reclaim Page');
