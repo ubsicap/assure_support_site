9e10d2a22ebc7cc5974c4f2f6828d638b16683cd
Danny Hammer <daniel_hammer@sil.org>
2022-07-13 08:38:22 -0400

    feat: Redirects to account page and updates users table:

--- public/qa-plugin/account-reclaim/qa-ar-overrides.php
+++ public/qa-plugin/account-reclaim/qa-ar-overrides.php
@@ -56,3 +56,62 @@ function qa_start_reset_user($userid, $reclaim = false)
         qa_start_reset_user_base($userid);
     }
 }
+
+
+
+/**
+ * Successfully finish the 'Reclaim Account' process for $userid, cleaning their reclaim table entry and logging in the user
+ * 
+ *  * This is an OVERRIDE. The original function is:
+ *      qa-include/app/users-edit.php:qa_finish_reset_user($userid, $newPassword)
+ * 
+ * @param mixed $userId The userid identifiying the user who will have the password reset
+ * @param string $newPassword The new password for the user
+ * @param string $newEmail Email address used in account reclaim
+ * @param string $newUsername The new username of the account, if applicable
+ * @return void
+ */
+function qa_finish_reset_user($userId, $newPassword, $newEmail = null, $newUsername = null)
+{
+    if ($newEmail != null && $newUsername != null) {
+        // For qa_db_user_set_password(), qa_db_user_set()
+        require_once QA_INCLUDE_DIR . 'db/users.php';
+
+        // For qa_set_logged_in_user()
+        require_once QA_INCLUDE_DIR . 'app/options.php';
+
+        // For qa_cookie_get()
+        require_once QA_INCLUDE_DIR . 'app/cookies.php';
+
+        // For qa_db_select_with_pending(), qa_db_user_account_selectspec()
+        require_once QA_INCLUDE_DIR . 'db/selects.php';
+
+        // For qa_set_logged_in_user()
+        require_once QA_INCLUDE_DIR . 'app/users.php';
+
+        // Setting the password is a different process than setting other fields
+        qa_db_user_set_password($userId, $newPassword);
+
+        // Set the fields of the account to the newly provided values
+        qa_db_user_set($userId, array(
+            'email' => $newEmail,       // Update the email address so the account is valid
+            'handle' => $newUsername,   // Update the username to no longer be `anon######`
+            'emailcode' => '',          // Prevent re-use of the code, if it exists
+        ));
+
+        // Delete this user's entry in the account reclaim table
+        $sql = 'DELETE FROM ^accountreclaim WHERE ^accountreclaim.userid=$';
+        qa_db_query_sub($sql, $userid);
+
+        $userInfo = qa_db_select_with_pending(qa_db_user_account_selectspec($userId, true));
+
+        qa_set_logged_in_user($userId, $userInfo['handle'], false, $userInfo['sessionsource']); // reinstate this specific session
+
+        // Report that a 'user reclaim' event has occurred (for event modules)
+        qa_report_event('u_reclaim', $userId, $userInfo['handle'], qa_cookie_get(), array(
+            'email' => $userInfo['email'],
+        ));
+    } else {
+        qa_finish_reset_user_base($userId, $newPassword);
+    }
+}
