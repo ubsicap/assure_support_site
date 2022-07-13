5e92c8cb2c33cd107489cd22e0d4271a49c4a2ed
Daniel March <daniel_march@sil.org>
2022-07-13 12:44:21 -0400

    Forgot to push one file

--- public/qa-plugin/account-reclaim/qa-ar-email.php
+++ public/qa-plugin/account-reclaim/qa-ar-email.php
@@ -17,7 +17,6 @@ class qa_ar_filter
     */
     public function filter_email(&$email, $olduser)
     {
-        return ('test <br> test');
         //check if email belongs to an archived account
         if(qa_ar_db_is_archived_email($email))
         {
