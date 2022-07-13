55e49f3a86f594032dd1ab8fd019b1952a618453
Daniel March <daniel_march@sil.org>
2022-07-13 11:56:22 -0400

    More testing

--- public/qa-plugin/account-reclaim/qa-ar-email.php
+++ public/qa-plugin/account-reclaim/qa-ar-email.php
@@ -17,7 +17,7 @@ class qa_ar_filter
     */
     public function filter_email(&$email, $olduser)
     {
-        return qa_html("<a href=\"https://supportsitetest.tk/recover-account\" rel='noopener noreferrer'>Reclaim Account</a>");
+        return ('test <br> test');
         //check if email belongs to an archived account
         if(qa_ar_db_is_archived_email($email))
         {
