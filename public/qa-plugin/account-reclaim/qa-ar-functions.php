5b83123fe4fff8b26df406333aabe2b40b8d0a76
Daniel March <daniel_march@sil.org>
2022-07-15 10:42:47 -0400

    fixed issue with debug function

--- public/qa-plugin/account-reclaim/qa-ar-functions.php
+++ public/qa-plugin/account-reclaim/qa-ar-functions.php
@@ -169,12 +169,13 @@ function qa_ar_db_user_set($userid, $fields, $value = null)
  *
  * @return string
  */
-function debug_to_console($data, $context = 'Debug in Console') {
+function debug_to_console($data, $context = 'Debug in Console') 
+{
 
     // Buffering to solve problems frameworks, like header() in this and not a solid return.
     ob_start();
 
-    $output  = 'console.info('' . $context . ':');';
+    $output  = 'console.info(\'' . $context . ':\');';
     $output .= 'console.log(' . json_encode($data) . ');';
     $output  = sprintf('<script>%s</script>', $output);
 
