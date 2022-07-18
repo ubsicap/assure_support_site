6347efda85be83446284da1160a80186b5b662a1
Daniel March <daniel_march@sil.org>
2022-07-18 15:43:33 -0400

    now with base directory passed

--- public/qa-plugin/q2a-role-markers/qa-marker-functions.php
+++ public/qa-plugin/q2a-role-markers/qa-marker-functions.php
@@ -3,11 +3,12 @@
 /**
  * Return the svg data of the badge icon, used for dynamic coloring
  *
+ * @param $baseDir = QA_HTML_THEME_LAYER_URLTOROOT
  * @return string
  */
-function qa_get_badge_svg()
+function qa_get_badge_svg($baseDir)
 {
-    $filePath = QA_HTML_THEME_LAYER_URLTOROOT . 'qa-shield-gen.svg';
+    $filePath = $baseDir . 'qa-shield-gen.svg';
     $svgFile = fopen($filePath, "r");
     $text = fread($svgFile,filesize($filePath));
     fclose($svgFile);
