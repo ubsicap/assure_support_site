76292c3c86f084deca78b4a23a7e8c5bbb8a6662
Daniel March <daniel_march@sil.org>
2022-07-19 10:57:27 -0400

    See previous

--- public/qa-plugin/sso-authentication/sso-authentication-layer.php
+++ public/qa-plugin/sso-authentication/sso-authentication-layer.php
@@ -56,6 +56,7 @@ class qa_html_theme_layer extends qa_html_theme_base
             border: 1px solid #31708f;
             border-radius: 25px;
             perspective: 1px;
+            overflow: hidden;
             margin-bottom: 8px;
             display: inline-block;
             border-radius: 1px;
