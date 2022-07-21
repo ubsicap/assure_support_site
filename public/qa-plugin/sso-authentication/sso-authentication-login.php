56be63b98395a6483a51fd1391d66eaee0cd4310
Daniel March <daniel_march@sil.org>
2022-07-21 11:08:16 -0400

    a couple more pushes to go

--- public/qa-plugin/sso-authentication/sso-authentication-login.php
+++ public/qa-plugin/sso-authentication/sso-authentication-login.php
@@ -103,7 +103,7 @@ class sso_authentication_login
 			try {
 				$url = 'https://www.googleapis.com/oauth2/v4/token';
 				// Get the access token 
-				$data = $this->getAccessToken($url, qa_opt('google_authentication_client_id'), qa_opt('site_url') . "https://supportsitetest.tk/index.php", qa_opt('google_authentication_client_secret'), $_GET['code']);
+				$data = $this->getAccessToken($url, qa_opt('google_authentication_client_id'), "https://supportsitetest.tk/index.php", qa_opt('google_authentication_client_secret'), $_GET['code']);
 
 				// Access Token
 				$access_token = $data['access_token'];
