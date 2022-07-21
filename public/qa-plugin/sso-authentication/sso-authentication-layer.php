3b018d737270fefd3a141ccb762a9b6cadbe5040
Daniel March <daniel_march@sil.org>
2022-07-21 12:59:25 -0400

    Fixing icons

--- public/qa-plugin/sso-authentication/sso-authentication-layer.php
+++ public/qa-plugin/sso-authentication/sso-authentication-layer.php
@@ -32,7 +32,7 @@ class qa_html_theme_layer extends qa_html_theme_base
             }
 
             .google-signin, .facebook-signin, .paratext-signin {
-                left: 30%
+                margin-left: 35%;
                 width: 30%;
                 height: 40px;
                 border-radius: 3px;
@@ -46,38 +46,28 @@ class qa_html_theme_layer extends qa_html_theme_base
                 opacity: 1;
             }
             
+            .google-signin-icon, .facebook-signin-icon, .paratext-signin-icon {
+                box-sizing: content-box;
+                position: absolute;
+                width: 36px;
+                height: 36px;
+                border-radius: 3px 0px 0px 3px;
+                margin: 2px 2px 2px 2px;
+                background-size: 28px;
+                background-position: 4px 4px;
+                background-repeat: no-repeat;
+            }
             .google-signin-icon {
                 background-image: url("https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png");
-                background-size: 18px;
-                background-repeat: no-repeat;
-                background-position: 11px;
-                width: 40px;
-                height: 40px;
-                background-color: #fff;
-                display: inline-block;
-                vertical-align: middle;
+                background-color: #ffffff;
             }
             .facebook-signin-icon {
                 background-image: url("https://upload.wikimedia.org/wikipedia/en/thumb/0/04/Facebook_f_logo_%282021%29.svg/150px-Facebook_f_logo_%282021%29.svg.png");
-                background-size: 18px;
-                background-repeat: no-repeat;
-                background-position: 11px;
-                width: 40px;
-                height: 40px;
-                background-color: #fff;
-                display: inline-block;
-                vertical-align: middle;
+                background-color: #293e6a;
             }
             .paratext-signin-icon {
                 background-image: url("https://registry.paratext.org/static/logo-pt9.png");
-                background-size: 18px;
-                background-repeat: no-repeat;
-                background-position: 11px;
-                width: 40px;
-                height: 40px;
-                background-color: #fff;
-                display: inline-block;
-                vertical-align: middle;
+                background-color: #607749;
             }
             
             .signin-text {
@@ -88,8 +78,7 @@ class qa_html_theme_layer extends qa_html_theme_base
                 font-weight: bold;
                 font-family: "Roboto", sans-serif;
                 color: #fff;
-                margin-left: 13px;
-                margin-right: 8px;
+                margin: 11px 8px 11px 53px;
             }
             
             .google-signin.fa:hover, .google-signin.fa:focus, .google-signin:hover, .google-signin:focus {
