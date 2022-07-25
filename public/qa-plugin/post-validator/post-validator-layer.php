4cf611fe08b5f6cf4ddb2feb08d4d4b81b347d05
Ling <lingxin_chen@sil.org>
2022-07-25 16:28:46 -0400

    Post validator
    Only works when ask question checking for email and title and tag only... push for test

--- public/qa-plugin/post-validator/post-validator-layer.php
+++ public/qa-plugin/post-validator/post-validator-layer.php
@@ -2,8 +2,43 @@
 
 class qa_html_theme_layer extends qa_html_theme_base
 {
-	
+	//not sure which is the best
 
+	function head_script()
+	{
+		qa_html_theme_base::head_script();
+		// check if plugin is enabled, only load js-css-files if tags are needed: ask and edit question page
+		if (qa_opt('post_validator_enabled') && ($this->template == 'ask' || isset($this->content['form_q_edit']))) {
+			// mobile identifier
+			$ismobile = qa_is_mobile_probably() ? 'true' : 'false';
+			$this->output('<script type="text/javascript">
+					var ismobile = ' . $ismobile . ';
+				</script>');
+			// $this->output('<script type="text/javascript" src="' . QA_PLUGIN_DIR . 'post-validator/script.js"></script>');
+			$this->output('<script type="text/javascript" src="' . QA_HTML_THEME_LAYER_URLTOROOT . 'script.js"></script>');
+			$this->output(' 
+	<style type="text/css">
+.post-validator-error {
+	color: #a94442;
+	margin-top: 10px;
+	margin-bottom: 10px;
+	font-size: 16px;
+}
+.qa-form-tall-table > tbody > tr > td.post-validator-error,
+.qa-form-wide-table > tbody > tr > td.post-validator-error{
+  border: 1px solid #ebccd1;
+  background-color: #f2dede;
+  color: #a94442;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  border-radius: 4px;
+}
+.qa-form-wide-table > tbody > tr > td.post-validator-error {
+  display: table-cell !important;
+}
+</style>');
+		}
+	}
 
 	// end qa_html_theme_layer
 }
---
+++ public/qa-plugin/post-validator/script.js
@@ -0,0 +1,52 @@
+$ (document).ready (function () {
+  $ ('#title').change (function () {
+    console.log("change")
+    var VAL = this.value;
+    var email = new RegExp (
+      "\\b[A-Za-z\\.0-9!#$%&'*+/=?^_`{|}~-]+@[A-Za-z1-9-]+\\.[A-Za-z0-9-]+\\b"
+    );
+    if (email.test (VAL)) {
+      if ($ ('#title').parent ().find ('.post-validator-error').length === 0) {
+        var warning = createWarning ('email');
+        $ ('#title').parent ().append (warning);
+      }
+    } else {
+      $ ('#title').parent ().find ('.post-validator-error').remove ();
+    }
+  });
+
+  //check for sensitive info in tagbox, either add or remove warning
+  $ ('.tagbox > ul').bind ('DOMSubtreeModified', function () {
+    var listItems = $ ('.tagbox > ul li');
+    var notFound = true;
+    listItems.each (function (idx, li) {
+      if (idx === listItems.length - 1) return false; //does not check for the input
+      var product = $ (li);
+      var VAL = product.children (':first').text (); //get the text of tag
+      var email = new RegExp (
+        "\\b[A-Za-z\\.0-9!#$%&'*+/=?^_`{|}~-]+@[A-Za-z1-9-]+\\.[A-Za-z0-9-]+\\b"
+      );
+      if (email.test (VAL)) {
+        notFound = false;
+        if (
+          $ ('.tagbox').parent ().find ('.post-validator-error').length === 0
+        ) {
+          var warning = createWarning ('email');
+          $ ('.tagbox').parent ().append (warning);
+          return false;
+        }
+      }
+    });
+    if (notFound) {
+      $ ('.tagbox').parent ().find ('.post-validator-error').remove ();
+    }
+  });
+});
+
+function createWarning (category) {
+  var warning =
+    '<div class="post-validator-error">Sensitive information detected: ' +
+    category +
+    '. Please refer to: <a href = "https://supportsitetest.tk/best-practices">our best practice page.</a></div>';
+  return warning;
+}
