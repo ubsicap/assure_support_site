e33309d74c291b312bd441d0203a36d0a9630c3a
Daniel March <daniel_march@sil.org>
2022-07-25 16:41:39 -0400

    okay try alternate method

--- public/qa-plugin/q2a-hashtagger/qa-hashtagger.php
+++ public/qa-plugin/q2a-hashtagger/qa-hashtagger.php
@@ -379,12 +379,9 @@ class qa_hashtagger
         if ($convert_hashtags) {
             //every other section should be in an html element (and ignored), i.e. hi,<there>,test,<div a>
             $sections = preg_split('%<[^>]*>%', $row['content'], -1, PREG_SPLIT_DELIM_CAPTURE); //split by html elements
-            if(count($sections) !=0 && $sections[0][0] == '<') //first section is an html element
-                $i = 1;
-            else //beginning is plain text
-                $i = 0;
-            for(; $i <= count($sections); $i=$i+2) //go through every other element
-                $sections[$i] = "S: " . $sections[$i] . " :E";    
+            for($i = 0; $i <= count($sections); $i++) //go through every other element
+                if($sections[$i][0] != '<') //not in a tag
+                    $sections[$i] = "S: " . $sections[$i] . " :E";    
             //$sections[$i] = "S: " . $this->preg_call('%#(?P<word>[\w\-]+)%us', 'build_tag_link', $sections[$i]) . " :E";
             $row['content'] = implode($sections); //piece back together the original html
         }
