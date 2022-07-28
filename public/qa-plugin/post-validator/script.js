3b4080bdf622dd5a1082eb2a9138f06dd1a764e2
Daniel March <daniel_march@sil.org>
2022-07-28 10:55:36 -0400

    Regex validator functions "should" be working

--- public/qa-plugin/post-validator/script.js
+++ public/qa-plugin/post-validator/script.js
@@ -84,10 +84,8 @@ $ (document).ready (function () {
 function checkField (text) {
 
   var warnings = '';
-  emailMatches = checkEmail (text);
-  if (
-    emailMatches != null //add all the warnings (if there are any)
-  )
+  emailMatches = checkEmail(text);
+  if (emailMatches != null) //add all the warnings (if there are any)
     for (var match of emailMatches)
       warnings = warnings + '<br>' + match;
 
@@ -96,13 +94,60 @@ function checkField (text) {
   else return createWarning (warnings); //format with warning message
 }
 
-function checkEmail (
-  text //return null (no match) or an array of all the matches
-) {
-  var matches;
-  var emailRegex = /\b[A-Za-z\.0-9!#$%&'*+/=?^_`{|}~-]+@[A-Za-z1-9-]+\.[A-Za-z0-9-]+\b/g;
-  return text.match (emailRegex);
+//regex functions, return null (no match) or an array of all the matches
+function checkEmail(text)
+{
+  //emails are in the format of begin@mid.end (each has different allowed characters)
+  var valRegex = /\b[A-Za-z\.0-9!#$%&'*+/=?^_`{|}~-]+@[A-Za-z1-9-]+\.[A-Za-z0-9-]+\b/g;
+  return text.match (valRegex);
 }
+function checkPhone(text) //phone number check, difficult as phone numbers vary in length often
+{
+  //phone numbers may be difficult as international phone numbers vary significantly, here we limit between 7 and 15 digits
+  var valRegex = /\b(\d[\s-.()]*){7,15}\b/g;
+  return text.match (valRegex);
+}
+function checkIP(text) //ip address address search
+{
+  //ip's in the format of num.num.num.num where number [0-255]
+  //regex checks for num.(3 times)num (where num is [0-999]), we validate after to make sure this is valid
+  var valRegex = /\b(\d{0,3}\s*\.\s*){3}\d{0,3}\b/g;
+  var matches = text.match(valRegex);
+  var finalMatches = []; //matches that are true ip addresses
+  for(var entry in matches) //go through each match  and check it is a true ip
+  {
+    var trueIp = true;
+    for(var seg in entry.split(".")) //each portion of the ip should be <= 255
+      if(parseInt(seg) > 255) //invalid, should be in range [0-255]
+        trueIp = false;
+    if(trueIp)
+      finalMatches.push(entry); //must be a falid ip
+  }
+  return finalMatches;
+}
+function checkMAC(text) //mac address 
+{
+  //mac address in the form byte:byte:byte:byte:byte:byte (6 secionts, each a byte in hex)
+  //regex like (byte:) 5 times then (byte), allow spaces in between sections
+  var valRegex = /\b([a-fA-f0-9]{2}\s*:\s*){5}[a-fA-f0-9]{2}\b/g;
+  return text.match (valRegex);
+}
+function checkRegistrationCode(text) //paratext registration code check i.e. AB1234-CDE23F-... (5 sections seperated by hyphens)
+{
+  //regex like: section-(5 times)section (spaces around hyphen allowed)
+  var valRegex = /\b([\da-zA-Z]{6}\s*-\s*){4}[\da-zA-Z]{6}\b/g;
+  return text.match (valRegex);
+}
+function checkImage(text) //check if an image is in the post (only returns true/false, not an array!)
+{
+  var valRegex = /<img[^>]*>/;
+  return text.match(valRegex)!=null; //true if an image element is in the text
+}
+function checkNames(text) //names may be added in the future, but currently no validation exists
+{
+  return null;
+}
+//end regex functions
 
 function createWarning (entries) {
   var warning =
