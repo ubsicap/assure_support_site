494bb8820c354b1f899116aa966c7d3c01e28ea8
Daniel March <daniel_march@sil.org>
2022-07-28 16:28:16 -0400

    make sure get_val_script function is found

--- public/qa-plugin/post-validator/post-validator-layer.php
+++ public/qa-plugin/post-validator/post-validator-layer.php
@@ -1,8 +1,33 @@
 <?php
 
+
+//helper function for head_script
+//get the main scripts needed to display the warnings, $for should either by ask or question, returns the script as a string
+function get_val_script($for)
+{
+	$scriptText = 
+	'<script type="text/javascript">
+		var enabled_name = ' . (qa_opt('post_val_name')=='1') . ';
+		var enabled_email = ' . (qa_opt('post_val_email')=='1') . ';
+		var enabled_phone = ' . (qa_opt('post_val_phone')=='1') . ';
+		var enabled_registration = ' . (qa_opt('post_val_registration')=='1') . ';
+		var enabled_ip = ' . (qa_opt('post_val_ip')=='1') . ';
+		var enabled_mac = ' . (qa_opt('post_val_mac')=='1') . ';
+		var enabled_images = ' . (qa_opt('post_val_images')=='1') . ';
+	</script>'; //all the option variables
+	$scriptText .= '<script type="text/javascript" src="' . QA_HTML_THEME_LAYER_URLTOROOT . 'utility.js"></script>'; //main functions
+
+	//attach the write script for either a question or a answer/comment
+	if($for == "ask") //for questions
+		$scriptText .= '<script type="text/javascript" src="' . QA_HTML_THEME_LAYER_URLTOROOT . 'script-ask.js"></script>';
+	elseif($for == "question") //for answers and comments
+		$scriptText .= '<script type="text/javascript" src="' . QA_HTML_THEME_LAYER_URLTOROOT . 'script-question.js"></script>';
+	return $scriptText;
+}
+
 class qa_html_theme_layer extends qa_html_theme_base
 {
-	function head_script()
+	function head_script() //attach scripts for displaying warnings
 	{
 		qa_html_theme_base::head_script();
 		// check if plugin is enabled, only load js-css-files if tags are needed: ask and edit question page
@@ -11,31 +36,6 @@ class qa_html_theme_layer extends qa_html_theme_base
 		elseif (qa_opt('post_validator_enabled') && ($this->template == 'question' && !isset($this->content['form_q_edit'])))
 			$this->output(get_val_script('question'));
 	}
-
-	//helper function for head_script
-	//get the main scripts needed to display the warnings, $for should either by ask or question, returns the script as a string
-	function get_val_script($for)
-	{
-		$scriptText = 
-		'<script type="text/javascript">
-			var enabled_name = ' . (qa_opt('post_val_name')=='1') . ';
-			var enabled_email = ' . (qa_opt('post_val_email')=='1') . ';
-			var enabled_phone = ' . (qa_opt('post_val_phone')=='1') . ';
-			var enabled_registration = ' . (qa_opt('post_val_registration')=='1') . ';
-			var enabled_ip = ' . (qa_opt('post_val_ip')=='1') . ';
-			var enabled_mac = ' . (qa_opt('post_val_mac')=='1') . ';
-			var enabled_images = ' . (qa_opt('post_val_images')=='1') . ';
-		</script>'; //all the option variables
-		$scriptText .= '<script type="text/javascript" src="' . QA_HTML_THEME_LAYER_URLTOROOT . 'utility.js"></script>'; //main functions
-
-		//attach the write script for either a question or a answer/comment
-		if($for == "ask") //for questions
-			$scriptText .= '<script type="text/javascript" src="' . QA_HTML_THEME_LAYER_URLTOROOT . 'script-ask.js"></script>';
-		elseif($for == "question") //for answers and comments
-			$scriptText .= '<script type="text/javascript" src="' . QA_HTML_THEME_LAYER_URLTOROOT . 'script-question.js"></script>';
-		return $scriptText;
-	}
-
 
 	function head_css()
     {
