f3d3c3d172da3dbd7ab63aab41c82a4d58e4e30b
Daniel March <daniel_march@sil.org>
2022-07-29 14:34:45 -0400

    add validation to new comment / reply

--- public/qa-plugin/post-validator/script-question.js
+++ public/qa-plugin/post-validator/script-question.js
@@ -1,6 +1,6 @@
 $ (document).ready (function () 
 {
-  //check sensitive info in answer in comments
+  //check sensitive info in answer
   $('.qa-q-view-buttons').children().last().click(function()
   {
     //check sensitive info in body
@@ -16,6 +16,41 @@ $ (document).ready (function ()
       console.log ('Failed to get editor!');
     });
   });
+
+  //check sensitive info in comment
+  $('.qa-a-item-buttons').children().last().click(function()
+  {
+    //check sensitive info in body
+    $.getScript ('/qa-plugin/wysiwyg-editor/ckeditor/ckeditor.js?1.8.6')
+    .done (function (script, textStatus) {
+      var interval = setInterval(function() {
+        $('iframe').contents ().find ('body').bind ('DOMSubtreeModified', function () {onCKEditor("ask");}); 
+        if ($("iframe").contents().find('body').length ) 
+          clearInterval(interval);
+      }, 100)
+      ;}) 
+    .fail (function (jqxhr, settings, exception) {
+      console.log ('Failed to get editor!');
+    });
+  });
+
+  //check sensitive info in reply (comment)
+  $('.qa-c-item-buttons').children().last().click(function()
+  {
+    //check sensitive info in body
+    $.getScript ('/qa-plugin/wysiwyg-editor/ckeditor/ckeditor.js?1.8.6')
+    .done (function (script, textStatus) {
+      var interval = setInterval(function() {
+        $('iframe').contents ().find ('body').bind ('DOMSubtreeModified', function () {onCKEditor("ask");}); 
+        if ($("iframe").contents().find('body').length ) 
+          clearInterval(interval);
+      }, 100)
+      ;}) 
+    .fail (function (jqxhr, settings, exception) {
+      console.log ('Failed to get editor!');
+    });
+  });
+
 
   /*
   $('.qa-a-item-buttons, .qa-c-item-buttons').children().first().click(function()
